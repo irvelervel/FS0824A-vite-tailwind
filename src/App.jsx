22aa609360a652212a1d0ae6d333789b893904e5
Stefano Casasola <ste.casasola@gmail.com>
2024-12-13 09:45:34 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MyNavbar from './components/MyNavbar'
+
+function App() {
+  return (
+    <>
+      <MyNavbar />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  )
+}
+
+export default App
